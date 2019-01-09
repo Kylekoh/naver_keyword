b0c81ef83f907e10d9e5735c0b23b7f5f559fdfb
고영일 <kyle_koh@goyeong-il-ui-MacBook-Pro.local>
2019-01-09 17:26:06 +0900

    fix get api using axios

---
+++ src/component/Form/Form.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import styles from './Form.scss';
+import axios from 'axios';
+
+const crypto = require('crypto');
+// import personal information from .env file.
+// const {
+//   API_KEY: api_key,
+//   CLIENT_KEY: client_key,
+//   CLIENT_NUM: client_num
+// } = process.env;
+const api_key = '0100000000509c9d1f7ee362d1adb639983214131b17cfc07768c57afee2951afcf4530215'
+const client_key = 'AQAAAACMi/RyVIV+oKKb3B555Pcc56lxF8yI0Y5NQ8WClK3j8A=='
+const client_num = '1295182'
+
+const time_stamp = new Date().getTime();
+
+class Form extends Component {
+  constructor() {
+    super();
+    this.generateHMAC = this.generateHMAC.bind(this);
+    this.state = {
+    }
+  }  
+
+  generateHMAC = (clientKey) => {
+    const method = "GET";
+    // 암호화 객체 생성, sha256 알고리즘 선택
+    const path = "/keywordstool"
+    const hmac = crypto.createHmac('sha256', clientKey);
+    const result = hmac.update(time_stamp + "." + method + "." + path).digest('base64');
+    
+    return result;
+  }
+
+  componentDidMount = () => {
+    axios.get('https://cors-anywhere.herokuapp.com/https://api.naver.com/keywordstool', {
+      headers: {
+        'X-API-KEY': api_key,
+        'X-Customer': client_num,
+        'X-Timestamp': time_stamp,
+        'X-Signature': this.generateHMAC(client_key),
+        'Content-Type': 'application/json',
+      },
+      params: {
+        "includeHintKeywords" : 0,
+        "showDetail" : 1,
+        "hintKeywords" : "주식투자",              
+      }
+    })
+    .then(function(response) {
+      console.log(response.data.keywordList)
+    })    
+  } 
+
+  render() {
+    return (
+      <div>
+        Form
+      </div>
+    );
+  }
+}
+
+export default Form;
---
+++ src/component/KeywordList/KeywordList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import styles from './KeywordList.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+class KeywordList extends Component {
+  render() {
+    return (
+      <div>
+        KeywordList
+      </div>
+    );
+  }
+}
+
+export default KeywordList;
